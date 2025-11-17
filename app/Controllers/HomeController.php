1f28cbd5fd7d5671a54d08b6a168881f644707b8
Agung Reza <168580913+AgungReza@users.noreply.github.com>
2025-11-17 12:35:46 +0700

    Agung (#5)

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,6 +6,12 @@ class HomeController extends BaseController
 {
         public function index()
     {
-        return view('dashboard/home');
+        $data = [
+            'title'      => 'Bulk Input',
+            'page_title' => 'Bulk Input',
+            'show_stats' => true, // jika ingin menampilkan sticky stats
+            // data lain untuk view
+        ];
+        return view('dashboard/home', $data);
     }
 }
