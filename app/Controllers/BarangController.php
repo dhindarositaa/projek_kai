342bebfea92f28e7326148f8e484c8aa17ada079
Agung Reza <168580913+AgungReza@users.noreply.github.com>
2025-11-17 12:35:46 +0700

    Agung (#5)

--- app/Controllers/BarangController.php
+++ app/Controllers/BarangController.php
@@ -6,6 +6,12 @@ class BarangController extends BaseController
 {
         public function index()
     {
-        return view('dashboard/barang');
+        $data = [
+            'title'      => 'Bulk Input',
+            'page_title' => 'Bulk Input',
+            'show_stats' => true, // jika ingin menampilkan sticky stats
+            // data lain untuk view
+        ];
+        return view('dashboard/barang', $data);
     }
 }
