ff7aaf6f74d542f816a70719b96ce1cc7c517974
AgungReza <jangkriktakbersuara@gmail.com>
2025-12-23 17:46:20 +0700

    chek up

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,6 +1,6 @@
 <footer class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 text-sm text-gray-500">
   <div class="flex items-center justify-between">
-    <div>© <?= date('Y') ?> PlainHabit</div>
+    <div>© <?= date('Y') ?> KAI SISTEM INFORMASI DAOP 6</div>
     <div>Versi 1.0</div>
   </div>
 </footer>
